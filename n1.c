791bea43079c441bd36408feab5c4b6ff0b53db5
Vinícius <viniciusvsl2219@gmail.com>
2026-09-19 14:54:58 -0300

    Atualizações do cardápio. ATT

--- n1.c
+++ n1.c
@@ -13,37 +13,59 @@ void menu(void){
     printf("\n4 - Relatório da sessão");
     printf("\n0 - Sair");
 }
-void carrinho(char nome[][50], float precos[], int *totalItens, float *valor){
-    if(*totalItens >= MAX_PRODUTOS){
-        printf("Carrinho Cheio!");
-        return;
-    }
-    else{
-        printf("Nome do produto: ");
-        scanf(" %[^\n]", nome[*totalItens]);
-        printf("Preço: ");
-        scanf("%f", &precos[*totalItens]);
 
-        *valor += precos[*totalItens];
-        (*totalItens)++;
-    }
-}
-void produtos(){
-    float sanduiche = 12.00, refrigerante = 6.00, suco = 8.00, salgado = 7.00, cafe = 4.00;
-    printf("Sanduíche: %.2f\n", sanduiche);
-    printf("Refrigerante: %.2f\n", refrigerante);
-    printf("Suco: %.2f\n", suco);
-    printf("Salgado: %.2f\n", salgado);
-    printf("Café: %.2f\n", cafe);
+void cardapio(){
+    printf("\n1- Sanduíche: R$12,00\n");
+    printf("2- Refrigerante: R$6,00\n");
+    printf("3- Suco: R$8,00\n");
+    printf("4- Salgado: R$7,00\n");
+    printf("5- Café: R$4,00\n");
 }
 void pedido(){
     char nome_cliente[50];
+    int continuar;
+    int opcao_cardapio;
     
     printf("\nDigite o nome do cliente: ");
     scanf(" %[^\n]", nome_cliente);
 
-    produtos();
-    carrinho(nome, precos, &totalItens, &valor);
+    do{
+        cardapio();
+
+        printf("Digite a opção do cardápio: (1 a 5): \n");
+        scanf("%d", &opcao_cardapio);
+        
+        float preco_unitario = 0.0;
+        
+        switch (opcao_cardapio)
+        {
+        case 1:
+            preco_unitario = 12.00;
+            break;
+        case 2:
+            preco_unitario = 6.00;
+            break;
+        case 3:
+            preco_unitario = 8.00;
+            break;
+        case 4:
+            preco_unitario = 7.00;
+            break;
+        case 5:
+            preco_unitario = 4.00;
+            break;
+        default:
+            printf("Produto inválido!\n");
+            continue;
+        }
+
+        printf("Digite a quantidade desejada: \n");
+
+
+        printf("\nDeseja continuar comprando? (1 - Sim || 2 - Não)\n");
+        scanf("%d", &continuar);
+    }while(continuar == 1);
+    printf("Pedido de %s finalizado com sucesso!!!\n", nome_cliente);
 }
 void calculadora(){
     float num1, num2;
@@ -54,11 +76,15 @@ void calculadora(){
     printf("2- Subtrair\n");
     printf("3- Multiplicar\n");
     printf("4- Dividir\n");
-    printf("0- Voltar")
+    printf("0- Voltar");
     
-    printf("Digite sua opção: \n");
+    printf("\nDigite sua opção: \n");
     scanf("%d", &opcao_calc);
-    if (opcao_calc < 0 || opcao_calc > 4)
+    if (opcao_calc < 0 || opcao_calc > 4){
+        printf("Digite uma opção válida!");
+        return;
+    }
+
     printf("Digite o primeiro número: \n");
     scanf("%f", &num1);
     printf("Digite o segundo número; \n");
@@ -99,7 +125,7 @@ void calculadora(){
 }
 int main(){
     int opcao;
-    char nome[MAX_PRODUTOS][50];
+    char nome_produto[MAX_PRODUTOS][50];
     float precos[MAX_PRODUTOS];
     int totalItens = 0;
     float valor = 0.0;
